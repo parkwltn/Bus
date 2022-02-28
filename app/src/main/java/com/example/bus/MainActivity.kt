538f2bfd743d9681c0dd6c0dbb3dbc100e7c594b
ZSU <zsueue@gmail.com>
2022-02-28 16:06:28 +0900

    0.0.4 routeIdToName

--- app/src/main/java/com/example/bus/MainActivity.kt
+++ app/src/main/java/com/example/bus/MainActivity.kt
@@ -20,10 +20,10 @@ class MainActivity : AppCompatActivity() {
         textView.text = ""
 
         //1분마다 도착정보 조회
-        timer (period = 60000) {
+        //timer (period = 60000) {
             val thread = NetworkThread();
             thread.start()
-        }
+        //}
 
     }
 
@@ -73,6 +73,29 @@ class MainActivity : AppCompatActivity() {
                         "200000055" -> routeId = "25"
                     }
 
+                    //버스번호 입력 받기 - HW 연동
+                    var enteredBusNum = "25"
+
+                    //if enteredBusNum in routeId
+                    if (enteredBusNum in routeId) {
+                        //탑승 예약 안내
+                        println("$enteredBusNum 번 버스 탑승 예약이 완료되었습니다.")
+
+                        //예약 리스트
+                        var BusReservationList = mutableListOf<String>()
+                        BusReservationList.add(enteredBusNum)
+                        //print(BusReservationList)
+
+                        //도착 안내 1: 등록 완료 후 바로 안내
+                        when (enteredBusNum) {
+                            "3000" -> println("$routeId 번 버스는 $predictTime1 분 후 도착합니다.")
+                            "25" -> println("$routeId 번 버스는 $predictTime1 분 후 도착합니다.")
+                        }
+
+                        //도착 안내 2: 1, 3, 6, 9분 전 안내
+                    }
+
+                    //테스트 - 삭제
                     runOnUiThread {
                         textView.append("노선Id: ${routeId}\n")
                         textView.append("도착예정시간1: ${predictTime1}\n")
